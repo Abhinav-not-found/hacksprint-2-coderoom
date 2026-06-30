8f1012405910d65c32061ad9d29ece7de292b591
Sumit <sumitk72004@gmail.com>
2026-06-30 15:10:11 +0530

    done changes

--- client/src/services/roomService.js
+++ client/src/services/roomService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8001/api/rooms";
+const API_BASE = "http://localhost:8000/api/rooms";
 
 export const createRoom = (payload) => {
 	return axios.post(`${API_BASE}/create`, payload);
