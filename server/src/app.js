814cec581e95e86cd91abd63d9f49d147cffde6f
Sumit <sumitk72004@gmail.com>
2026-06-30 11:49:11 +0530

    done

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import config from "./config/config.js";
 import errorHandler from "./middlewares/error.handler.js";
+import roomRouter from "./routes/room.routes.js";
 
 const app = express();
 app.use(express.json({ limit: "15kb" }));
